e2a1894a40cd5cd2cd1632c868c116a230e07a1b
Erwan Keribin <ektech@gmx.com>
2022-12-07 07:33:34 +0100

    [day7] Add solution scaffold

---
+++ day7/no_space_left_on_device.rb
@@ -0,0 +1,31 @@
+require "active_support/all"
+
+class NoSpaceLeftOnDevice
+  attr_accessor :input
+
+  def initialize(input_filename)
+    @input = File.read(input_filename).split("\n")
+  end
+
+  def solve1
+    "not implemented yet"
+  end
+
+  def solve2
+    "not implemented yet"
+  end
+
+  def self.run
+    source_folder = File.dirname(__FILE__)
+    example_solver = new("#{source_folder}/example")
+    puzzle_solver = new("#{source_folder}/puzzle")
+    puts "Solving first part :"
+    puts "=> example solution : #{example_solver.solve1}"
+    puts "=> puzzle solution : #{puzzle_solver.solve1}"
+    puts "Solving second part :"
+    puts "=> example solution : #{example_solver.solve2}"
+    puts "=> puzzle solution : #{puzzle_solver.solve2}"
+  end
+end
+
+NoSpaceLeftOnDevice.run if __FILE__ == $0
