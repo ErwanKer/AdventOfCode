7dccda7798db1f867e6915df2aff706af003f537
Erwan Keribin <ektech@gmx.com>
2022-12-08 09:58:44 +0100

    [day8] WIP solution for first part

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ gem "nokogiri", "~> 1.13"
 gem "thor", "~> 1.2"
 
 gem "rubytree", "~> 2.0"
+
+gem "daru", "~> 0.3"
--- day8/treetop_tree_house.rb
+++ day8/treetop_tree_house.rb
@@ -1,14 +1,47 @@
 require "active_support/all"
+require "daru"
+
+class Forest < Daru::DataFrame
+  attr_reader :width, :height
+
+  def initialize(matrix)
+    super(matrix.map.with_index.to_h.invert)
+    @width = size
+    @height = vectors.size
+    self
+  end
+
+  def inspect
+    map { |r| r.to_a.map(&:to_s).join }.join("\n")
+  end
+
+  def clone
+    self.class.new(transpose.to_a.first)
+  end
+
+  def count(el)
+    map { |v| v.count(el) }.sum
+  end
+end
 
 class TreetopTreeHouse
-  attr_accessor :input
+  attr_accessor :forest
 
   def initialize(input_filename)
-    @input = File.read(input_filename).split("\n")
+    @forest = Forest.new(File.readlines(input_filename, chomp: true).map(&:chars))
+  end
+
+  def set_visibility(forest)
+    forest[0] = ["V"] * forest.width
+    forest[forest.height - 1] = ["V"] * forest.width
+    forest.row[0] = ["V"] * forest.height
+    forest.row[forest.width - 1] = ["V"] * forest.height
+    forest
   end
 
   def solve1
-    "not implemented yet"
+    vforest = set_visibility(forest.clone)
+    vforest.count("V")
   end
 
   def solve2
