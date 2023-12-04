dabc2e500db5f083926266ab25b9809788f0654e
Erwan Keribin <ektech@gmx.com>
2023-12-04 17:14:45 +0100

    [2023-day2] Add solution scaffold

---
+++ AdventOfCode2023/day02/cube_conundrum_spec.rb
@@ -0,0 +1,19 @@
+require_relative "./cube_conundrum"
+
+RSpec.describe CubeConundrum do
+  subject { described_class.new(example, direct_input: true) }
+  describe ".solve1" do
+    context "puzzle example" do
+      let(:example) do
+        <<~EXAMPLE
+          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        EXAMPLE
+      end
+      it { expect(subject.solve1).to eq(8) }
+    end
+  end
+end
