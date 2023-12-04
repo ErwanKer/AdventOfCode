d36b72b4cf5d0957ab4b5c27601b31af5c62fce6
Erwan Keribin <ektech@gmx.com>
2023-12-04 17:40:25 +0100

    [2023-day2] Add solution to first part of the puzzle

--- AdventOfCode2023/day02/cube_conundrum.rb
+++ AdventOfCode2023/day02/cube_conundrum.rb
@@ -2,14 +2,39 @@ require "active_support/all"
 require "byebug"
 
 class CubeConundrum
-  attr_accessor :input
+  attr_accessor :games
+
+  class Cubes
+    attr_accessor :red, :green, :blue
+
+    def initialize(input_line)
+      vals = input_line.split(",").map do |number_color|
+        number, color = number_color.strip.split
+        [color, number.to_i]
+      end.to_h
+      @red = vals["red"] || 0
+      @green = vals["green"] || 0
+      @blue = vals["blue"] || 0
+    end
+
+    def possible?(ref_cubes)
+      red <= ref_cubes.red && green <= ref_cubes.green && blue <= ref_cubes.blue
+    end
+  end
 
   def initialize(input, direct_input: false)
-    @input = (direct_input ? input : File.read(input)).split("\n")
+    input = (direct_input ? input : File.read(input)).split("\n")
+    @games = input.map do |line|
+      game_id, game_cubes = line.split(":").map(&:strip)
+      [game_id.split.last.to_i, game_cubes.split(";").map { |gc| Cubes.new(gc)}]
+    end.to_h
   end
 
   def solve1
-    "not implemented yet"
+    ref_cubes = Cubes.new("12 red, 13 green, 14 blue")
+    games.select do |_, game_cubes|
+      game_cubes.all? { |game| game.possible?(ref_cubes) }
+    end.keys.sum
   end
 
   def solve2
