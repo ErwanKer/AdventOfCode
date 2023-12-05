68140a8597a169e218eae2792761493fa10c570a
Erwan Keribin <ektech@gmx.com>
2023-12-05 08:54:55 +0100

    [2023-day3] Add solution scaffold

---
+++ AdventOfCode2023/day03/gear_ratios_spec.rb
@@ -0,0 +1,44 @@
+require_relative "./gear_ratios"
+
+RSpec.describe GearRatio do
+  subject { described_class.new(example, direct_input: true) }
+  describe ".solve1" do
+    context "puzzle example" do
+      let(:example) do
+        <<~EXAMPLE
+          467..114..
+          ...*......
+          ..35..633.
+          ......#...
+          617*......
+          .....+.58.
+          ..592.....
+          ......755.
+          ...$.*....
+          .664.598..
+        EXAMPLE
+      end
+      it { expect(subject.solve1).to eq(4361) }
+    end
+  end
+
+  describe ".solve2" do
+    context "puzzle example" do
+      let(:example) do
+        <<~EXAMPLE
+          467..114..
+          ...*......
+          ..35..633.
+          ......#...
+          617*......
+          .....+.58.
+          ..592.....
+          ......755.
+          ...$.*....
+          .664.598..
+        EXAMPLE
+      end
+      it { expect(subject.solve2).to eq("???") }
+    end
+  end
+end
