9c4757a039a1043ee220f38bf30960e31be987d3
Erwan Keribin <ektech@gmx.com>
2023-12-06 10:08:57 +0100

    [2023-day6] Add solution scaffold

---
+++ AdventOfCode2023/day06/wait_for_it_spec.rb
@@ -0,0 +1,28 @@
+require_relative "./wait_for_it"
+
+RSpec.describe WaitForIt do
+  subject { described_class.new(example, direct_input: true) }
+  describe ".solve1" do
+    context "puzzle example" do
+      let(:example) do
+        <<~EXAMPLE
+          Time:      7  15   30
+          Distance:  9  40  200
+        EXAMPLE
+      end
+      it { expect(subject.solve1).to eq(288) }
+    end
+  end
+
+  describe ".solve2" do
+    context "puzzle example" do
+      let(:example) do
+        <<~EXAMPLE
+          Time:      7  15   30
+          Distance:  9  40  200
+        EXAMPLE
+      end
+      it { expect(subject.solve2).to eq("???") }
+    end
+  end
+end
