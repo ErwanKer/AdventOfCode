14618c2fbca34fbc7d63726e0af123c9a3d2a608
Erwan Keribin <ektech@gmx.com>
2023-12-06 16:19:36 +0100

    [2023-day05] Add solution to first part of the puzzle

--- AdventOfCode2023/day05/if_you_give_a_seed_a_fertilizer.rb
+++ AdventOfCode2023/day05/if_you_give_a_seed_a_fertilizer.rb
@@ -2,14 +2,58 @@ require "active_support/all"
 require "byebug"
 
 class IfYouGiveASeedAFertilizer
-  attr_accessor :input
+  attr_accessor :seeds, :maps
+
+  class Mapping
+    Range = Struct.new(:dst, :end)
+
+    def initialize(lines)
+      @src_idx = []
+      @mapping = {}
+      lines.each do |line|
+        dst, src, len = line.split.map(&:to_i)
+        @src_idx.append(src)
+        @mapping[src] = Range.new(dst, src + len)
+      end
+      @src_idx.sort!
+    end
+
+    def [](key)
+      possible_mapping_idx = @src_idx.bsearch_index { |val| val > key } || @src_idx.size
+      return key unless possible_mapping_idx.positive?
+
+      src = @src_idx[possible_mapping_idx - 1]
+      range = @mapping[src]
+      if key < range.end
+        range.dst + (key - src)
+      else
+        key
+      end
+    end
+  end
+
+  Map = Struct.new(:source, :destination, :mapping)
 
   def initialize(input, direct_input: false)
-    @input = (direct_input ? input : File.read(input)).split("\n")
+    sections = (direct_input ? input : File.read(input)).split("\n\n")
+    @seeds = sections.shift.split(":").last.split.map(&:to_i)
+    @maps = sections.map do |section|
+      lines = section.split("\n")
+      src, dst = lines.shift.split.first.split("-to-")
+      [src, Map.new(src, dst, Mapping.new(lines))]
+    end.to_h
   end
 
   def solve1
-    "not implemented yet"
+    seeds.map do |seed|
+      curr_map = maps["seed"]
+      curr_val = seed
+      while curr_map.destination != "location"
+        curr_val = curr_map.mapping[curr_val]
+        curr_map = maps[curr_map.destination]
+      end
+      curr_map.mapping[curr_val]
+    end.min
   end
 
   def solve2
