780982a58c19af616f1441f5f4ed3d4ef8309131
Erwan Keribin <ektech@gmx.com>
2023-12-07 08:59:12 +0100

    [2023-day07] Add solution to first part of the puzzle

--- AdventOfCode2023/day07/camel_cards.rb
+++ AdventOfCode2023/day07/camel_cards.rb
@@ -2,14 +2,88 @@ require "active_support/all"
 require "byebug"
 
 class CamelCard
-  attr_accessor :input
+  attr_accessor :hands
+
+  class Card
+    attr_reader :index, :letter
+
+    ORDER = "23456789TJQKA".chars
+
+    def initialize(letter)
+      @letter = letter
+      @index = ORDER.index(letter)
+    end
+
+    def <=>(other)
+      @index <=> other.index
+    end
+
+    def ==(other)
+      @letter == other.letter
+    end
+
+    def inspect
+      "#<Card: #{@letter}>"
+    end
+  end
+
+  class Hand
+    attr_reader :hand
+
+    TYPE_ORDER = ["High Card", "One pair", "Two pair", "Three of a kind", "Full house", "Four of a kind", "Five of a kind"]
+
+    def initialize(five_letters)
+      @hand = five_letters.chars.map { |letter| Card.new(letter) }
+    end
+
+    def index
+      @index ||= TYPE_ORDER.index(type)
+    end
+
+    def type
+      @type ||= begin
+        card_nums = hand.map(&:letter).tally.values.sort.reverse
+        case card_nums
+        in [5]
+          "Five of a kind"
+        in [4, 1]
+          "Four of a kind"
+        in [3, 2]
+          "Full house"
+        in [3, 1, 1]
+          "Three of a kind"
+        in [2, 2, 1]
+          "Two pair"
+        in [2, 1, 1, 1]
+          "One pair"
+        else
+          "High Card"
+        end
+      end
+    end
+
+    def <=>(other)
+      order = index <=> other.index
+      order.zero? ? hand <=> other.hand : order
+    end
+
+    def inspect
+      "#<Hand: #{hand.map(&:letter).join("")}>"
+    end
+  end
 
   def initialize(input, direct_input: false)
-    @input = (direct_input ? input : File.read(input)).split("\n")
+    lines = (direct_input ? input : File.read(input)).split("\n")
+    @hands = lines.map do |line|
+      hand, bid = line.split
+      [Hand.new(hand), bid.to_i]
+    end
   end
 
   def solve1
-    "not implemented yet"
+    hands.sort.map.with_index do |(hand, bid), index|
+      (index + 1) * bid
+    end.sum
   end
 
   def solve2
