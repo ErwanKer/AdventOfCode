e2a661b86afa8f622c8032641c0234bcc4a240e6
Erwan Keribin <ektech@gmx.com>
2023-12-12 09:57:18 +0100

    [2023-day12] Add solution to first part of the puzzle

--- AdventOfCode2023/day12/hot_springs.rb
+++ AdventOfCode2023/day12/hot_springs.rb
@@ -7,11 +7,27 @@ class HotSpring < Solver
 
   def initialize(input, direct_input: false)
     lines = (direct_input ? input : File.read(input)).split("\n")
-    @input = lines
+    @input = lines.map(&:split).map { |a, b| [a, b.split(",").map(&:to_i)] }
+  end
+
+  def possible_seqs(seq, start: "")
+    return start if seq.empty?
+
+    if seq[0] == "?"
+      [possible_seqs(seq[1..], start: start + "."), possible_seqs(seq[1..], start: start + "#")].flatten
+    else
+      possible_seqs(seq[1..], start: start + seq[0])
+    end
+  end
+
+  def possible_arrangement?(seq, con_list)
+    seq.split(".").select(&:present?).map(&:size) == con_list
   end
 
   def solve1
-    "not implemented yet"
+    input.map do |seq, con_list|
+      possible_seqs(seq).select { |s| possible_arrangement?(s, con_list) }.size
+    end.sum
   end
 
   def solve2
