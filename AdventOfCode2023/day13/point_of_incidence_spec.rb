2686456acf2da35d9505027852f75bf4ee67668e
Erwan Keribin <ektech@gmx.com>
2023-12-13 10:38:18 +0100

    [2023-day13] Add solution scaffold

---
+++ AdventOfCode2023/day13/point_of_incidence_spec.rb
@@ -0,0 +1,42 @@
+require_relative "./point_of_incidence"
+
+RSpec.describe PointOfIncidence do
+  let(:puzzle_example) do
+    <<~EXAMPLE
+      #.##..##.
+      ..#.##.#.
+      ##......#
+      ##......#
+      ..#.##.#.
+      ..##..##.
+      #.#.##.#.
+      
+      #...##..#
+      #....#..#
+      ..##..###
+      #####.##.
+      #####.##.
+      ..##..###
+      #....#..#
+    EXAMPLE
+  end
+  let(:puzzle_input) { File.read("#{File.dirname(__FILE__)}/puzzle") }
+
+  subject { described_class.new(example, direct_input: true) }
+
+  describe ".solve1" do
+    context "puzzle example" do
+      let(:example) { puzzle_example }
+
+      it { expect(subject.solve1).to eq(405) }
+    end
+  end
+
+  describe ".solve2" do
+    context "puzzle example" do
+      let(:example) { puzzle_example }
+
+      it { expect(subject.solve2).to eq("???") }
+    end
+  end
+end
